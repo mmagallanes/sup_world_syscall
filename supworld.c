cd5aa3dcf0db818b9200d914975260d71538503e
mmagallanes <mmagall5@lion.lmu.edu>
2019-04-06 23:08:19 -0700

    Add files via upload

--- supworld.c
+++ supworld.c
@@ -1,1 +1,9 @@
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/syscalls.h>
+#include <linux/linkage.h>
 
+asmlinkage int sys_supworld(void) {
+	printk(KERN_EMERG "Sup World");
+	return 0;
+}
